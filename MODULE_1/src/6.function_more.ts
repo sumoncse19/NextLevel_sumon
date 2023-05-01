c7c08bf458bc16f01961c96eb9d98c9df1500649
sumoncse19 <mdsumoncse19@gmail.com>
2023-05-01 18:46:09 +0600

    More_about_function

--- MODULE_1/src/6.function_more.ts
+++ MODULE_1/src/6.function_more.ts
@@ -14,6 +14,10 @@ defaultParameter(20)
 // spread operator
 const friends = ['anik', 'rabbul', 'joy']
 const newFriends = ['rachel', 'anika', 'joya']
+const myFriend = {
+  fullName: 'Abul Bashar',
+  age: 25,
+}
 
 // friends.push(newFriends); // --> এটা use করলে JS এ পুরো newFriends array টা friends array এর মধ্যে array আকারে বসে যেতো, কিন্তু আমরা এখনে চাচ্ছি শুধু নাম গুলা বসাতে এক্ষেত্রে split operator(...) use করতে হবে।
 
@@ -23,6 +27,23 @@ console.log(friends, 'my friends');
 
 
 // rest parameter
-const greetFriends = (friend1: string, friend2: string, friend3: string): void => console.log(`Hi ${friend1}, ${friend2}, ${friend3}`);
+const greetFriends = (
+  friend1: string,
+  friend2: string,
+  friend3: string): void => console.log(`Hi ${friend1}, Hi ${friend2}, Hi ${friend3}`);
 
-greetFriends('Abul', 'Babul', 'Cabul')
+greetFriends('Abul', 'Babul', 'Cabul')  // এক্ষেত্রে আমরা এক্সট্রা কোন friend add করতে পারবো না, শুধু ৩জন কেই add করতে পারবো। তাই আমরা যাতে আমাদের ইচ্ছা মতো এড করতে পারি সে জন্য rest operator use করবো।
+
+// const greetFriendsWithRest = (...friends: string[]): void => console.log(friends);
+const greetFriendsWithRest = (...friends: string[]): void => friends.forEach(friend => console.log(`Hi ${friend}`));
+
+greetFriendsWithRest('Abul', 'Babul', 'Cabul', 'Dabul', 'Ebul', 'Fibul', 'Hibul')
+
+
+// array and object destructure
+const [bestFriend] = friends
+const { fullName } = myFriend // এখানে fullName টা myFriend থেকে automatically এর টাইপ টা বুঝে নেয়, কিন্তু এরপরও যদি আমরা টাইপ ডেকলার করে দি তাইলে এটা দ্বারা ফুল নেইম কে access করা বুঝায়, ভ্যালু same এই থাকবে
+const { fullName: stringy } = myFriend // এখানে বোঝাচ্ছে যে আগে fullName এ যা ছিলো এখনো তাই আছে number হলে number বা string হলে string, কিন্তু এখানে এটাকে access করতে হলে আমরা এটাকে string এর মাধ্যমে access করতে পারবো, এক কথায় fullName এর আরেকটা নাম।
+
+console.log(stringy);
+console.log({ stringy });
